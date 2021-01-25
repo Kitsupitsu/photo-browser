5746dcc1b63383c7ce983e7ef711dc91fdaa7d38
Kiti Suupohja <kitsu@kapsi.fi>
2021-01-25 12:35:17 +0200

    Clean a bit

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import './Gallery.scss';
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Gallery extends Component {
     render() {
         const photos = this.props.photos
         return (
-            <div>
-                <div className="galleryGrid">
-                    {photos.map(photo => (
-                        <div id="galleryImage" key={photo.id}>
-                            <Link to={`/${photo.id}`}>
-                                <img src={photo.thumbnailUrl} />
-                            </Link>
-                        </div>
-                    ))}
-                </div>
+            <div className="galleryGrid">
+                {photos.map(photo => (
+                    <div id="galleryImage" key={photo.id}>
+                        <Link to={`/${photo.id}`}>
+                            <img src={photo.thumbnailUrl} />
+                        </Link>
+                    </div>
+                ))}
             </div>
         )
     }
