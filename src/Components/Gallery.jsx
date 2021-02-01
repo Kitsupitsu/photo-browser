8cb8692742861fdfd388cea2622fbeba732f2f9b
Kiti Suupohja <kitsu@kapsi.fi>
2021-02-01 20:00:35 +0200

    Add filtering to pictures

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Gallery.scss';
 import { Link } from "react-router-dom";
 
 const Gallery = (props) => {
-    const photos = props.photos
+    const photos = props.photos;
+    const [filter, setFilter] = useState("");
+
+    const filterPictures = (event) => {
+        setFilter(event.target.value);
+    }
+
+    const filteredPictures = photos.filter(item => {
+        return Object.keys(item).some(key => 
+            item[key].toString().includes(filter))
+    });
 
     return (
+        <>
+        <input id="input" value={filter} onChange={filterPictures} />
         <div className="galleryGrid">
-            {photos.map(photo => (
+            {filteredPictures.map(photo => (
                 <div id={"galleryImage-" + photo.id} key={photo.id} aria-label={"Move to photo" + photo.id}>
                     <Link to={`/photo/${photo.id}`}>
                         <img src={photo.thumbnailUrl} aria-label={"Photo id " + photo.id}/>
@@ -15,6 +27,7 @@ const Gallery = (props) => {
                 </div>
             ))}
         </div>
+        </>
     )
 }
 
